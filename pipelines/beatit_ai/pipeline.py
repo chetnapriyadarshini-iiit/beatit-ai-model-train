7a67d89da1b7152f2a4b74ccf733a74d639f51f0
chetna <chetna.eml61@iiitb.net>
2025-12-03 08:32:29 +0000

    Updated source dir
    sagemaker still does not recgonize helper dir, joining to base_dir

--- pipelines/beatit_ai/pipeline.py
+++ pipelines/beatit_ai/pipeline.py
@@ -159,7 +159,7 @@ def get_pipeline(
             ProcessingOutput(output_name="test", source="/opt/ml/processing/test"),
         ],
         code="preprocess.py",
-        source_dir="helper/",
+        source_dir= os.path.join(BASE_DIR, "helper/"),
         arguments=[
             "--raw-data-dir",
             "/opt/ml/processing/raw",
