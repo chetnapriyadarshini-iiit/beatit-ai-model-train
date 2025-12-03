1603aa227645c2bea54601a022e0a5344b6fc393
chetna <chetna.eml61@iiitb.net>
2025-12-03 16:26:21 +0000

    changes to code ref

--- pipelines/beatit_ai/pipeline.py
+++ pipelines/beatit_ai/pipeline.py
@@ -161,7 +161,7 @@ def get_pipeline(
             ProcessingOutput(output_name="validation", source="/opt/ml/processing/validation"),
             ProcessingOutput(output_name="test", source="/opt/ml/processing/test"),
         ],
-        code="preprocess.py",
+        code=os.path.join(BASE_DIR, "preprocess.py"),
         arguments=[
             "--raw-data-dir",
             "/opt/ml/processing/raw",
